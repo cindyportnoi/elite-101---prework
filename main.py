cd855bc03a0cd28f01c968d0d97a3b2cbd8e5919
cindyportnoi <cmportnoi@gmail.com>
2025-10-01 02:47:18 +0000

    added options placeholders

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-print("Welcome to the thrift store Chatbot")
+print("Welcome to the Elite 101 Chatbot!")
 name = input("What is your name? ")
 age = input("Hello " + name + ", how old are you? ")
+print("Wow, " + age + ", how young! How may I help you today?")
+print()
+print("Please choose one of these following options: ")
+print("1. Placeholder")
+print("2. Placeholder")
+print("3. Placeholder")
+print("4. Exit the conversation")
+choice = input("Option number: ")
+if choice == "1":
+    print()
+elif choice == "2":
+    print()
+elif choice == "3":
+    print()
+elif choice == "4":
+    print("Have a good day, " + name + "!")
+else:
+    print("Sorry, that's not one of the choices.")
